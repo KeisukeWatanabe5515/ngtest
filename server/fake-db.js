d14f3a65b4698d6edb73343b83346d177fd8f7cc
KeisukeWatanabe <leslesnoa5@gmail.com>
2020-09-30 01:03:04 +0900

    Nodeサーバー構築

---
+++ server/fake-db.js
@@ -0,0 +1,105 @@
+const Product = require('./model/product');
+
+class FakeDb {
+
+    constructor() {
+        this.products = [
+                {
+                  coverImage: '.\\assets\\img\\phone-cover.jpg',
+                  name: 'Phone XL',
+                  price: 799,
+                  description: 'A large phone with one of the best screens',
+                  heading1: {
+                    name: 'サンプルヘッダー1',
+                    text: 'サンプルテキスト1'
+                  },
+                  heading2: {
+                    name: 'サンプルヘッダー2',
+                    text: 'サンプルテキスト2'
+                  },
+                  heading3: {
+                    name: 'サンプルヘッダー3',
+                    text: 'サンプルテキスト3'
+                  },
+                },
+                {
+                  coverImage: 'data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==',
+                  name: 'Phone Mini',
+                  price: 699,
+                  description: 'A great phone with one of the best cameras',
+                  heading1: {
+                    name: 'サンプルヘッダー1',
+                    text: 'サンプルテキスト1'
+                  },
+                  heading2: {
+                    name: 'サンプルヘッダー2',
+                    text: 'サンプルテキスト2'
+                  },
+                  heading3: {
+                    name: 'サンプルヘッダー3',
+                    text: 'サンプルテキスト3'
+                  },
+                },
+                {
+                  coverImage: '.\\assets\\img\\phone-cover.jpg',
+                  name: 'Phone Standard',
+                  price: 299,
+                  description: '',
+                  heading1: {
+                    name: 'サンプルヘッダー1',
+                    text: 'サンプルテキスト1'
+                  },
+                  heading2: {
+                    name: 'サンプルヘッダー2',
+                    text: 'サンプルテキスト2'
+                  },
+                  heading3: {
+                    name: 'サンプルヘッダー3',
+                    text: 'サンプルテキスト3'
+                  },
+                },
+                {
+                  coverImage: '.\\assets\\img\\phone-cover.jpg',
+                  name: 'Phone Special',
+                  price: 299,
+                  description: '',
+                  heading1: {
+                    name: 'サンプルヘッダー1',
+                    text: 'サンプルテキスト1'
+                  },
+                  heading2: {
+                    name: 'サンプルヘッダー2',
+                    text: 'サンプルテキスト2'
+                  },
+                  heading3: {
+                    name: 'サンプルヘッダー3',
+                    text: 'サンプルテキスト3'
+                  },
+                }
+        ]
+    }
+
+    async initDb() {
+      await this.cleanDb()
+      this.pushProductsToDb()
+    }
+
+    async cleanDb() {
+      await Product.deleteMany({})
+    }
+
+    pushProductsToDb() {
+        this.products.forEach(
+            (product) => {
+                const newProduct = new Product(product);
+                newProduct.save();
+            }
+        )
+    }
+
+    seeDb() {
+        this.pushProductsToDb();
+    }
+}
+
+module.exports = FakeDb
