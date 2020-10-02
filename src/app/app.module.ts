6c2abf1a2190fa9a6f7d8f886bfbe13a2f1d77af
KeisukeWatanabe <leslesnoa5@gmail.com>
2020-10-03 00:12:15 +0900

    フロント側とバックエンド側の結合

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductService } from './product/shared/product.service';
+import { HttpClientModule } from '@angular/common/http';
 // import { ProductListComponent } from './product/product-listings/product-listings.component';
 // import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 // import { ProductComponent } from './product/product.component';
@@ -18,9 +20,12 @@ import { NavbarComponent } from './common/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,18 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../../products';
+// import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-listings',
   templateUrl: './product-listings.component.html',
-  styleUrls: ['./product-listings.component.scss']
+  styleUrls: ['./product-listings.component.scss'],
 })
 export class ProductListComponent implements OnInit {
   products: any;
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
-      this.products = products;
+  ngOnInit() {
+    // this.products = this.productService.getProducts();
+
+    const productsObservable = this.productService.getProducts();
+    productsObservable.subscribe(
+      (data) => {
+        this.products = data;
+        // console.log('次のデータが出力されました：' + data)
+        // debugger
+      },
+      (err) => { console.error('次のエラーが発生しました: ' + err) }
+    )
+
+    // const observable = new Observable((subscriber) => {
+    //   subscriber.next(1);
+    //   subscriber.next(2);
+    //   subscriber.next(3);
+    //   setTimeout(() => {
+    //     subscriber.next(4);
+    //     subscriber.complete();
+    //   }, 1000);
+    // });
+
+    // console.log('subscribe前');
+    // observable.subscribe({
+    //   next(data) { console.log('次のデータが出力されました：' + data); },
+    //   error(err) { console.error('次のエラーが発生しました: ' + err); },
+    //   complete() { console.log('完了しました！'); }
+    // });
+    // console.log('subscribeから抜けました！');
   }
 }
